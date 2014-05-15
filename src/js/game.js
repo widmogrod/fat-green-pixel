1c5de5c2b54a7f3331ce7870a51c77d730e87fa4
Krzysztof Kula <krzychukula@gmail.com>
2014-05-15 06:41:51 +0200

    add move mechanic

--- src/js/game.js
+++ src/js/game.js
@@ -16,12 +16,16 @@
       this.player = this.add.sprite(x, y, 'player');
       this.player.anchor.setTo(0.5, 0.5);
       this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
+      this.player.debug = true;
       this.player.body.collideWorldBounds = true;
+      this.game.camera.follow(this.player);
+
 
 
 
     //  The 'mario' key here is the Loader key given in game.load.tilemap
     this.tilemap = this.add.tilemap('sides-map');
+    this.tilemap.setCollision([9, 20])
 
     //  The first parameter is the tileset name, as specified in the Tiled map editor (and in the tilemap json file)
     //  The second parameter maps this name to the Phaser.Cache key 'tiles'
@@ -31,23 +35,47 @@
     //  A Layer is effectively like a Phaser.Sprite, so is added to the display list.
     this.layer = this.tilemap.createLayer('box-sides-layer');
     this.layer.debug = true;
+    //this.layer.fixedToCamera = true;
     //  This resizes the game world to match the layer dimensions
-    //this.layer.resizeWorld();
+    this.layer.resizeWorld();
+
+    this.player.position.y = this.tilemap.heightInPixels - 100;
+    this.game.camera.y =  this.tilemap.heightInPixels - 100;
+
+    console.log(this.tilemap.heightInPixels)
 
 
     },
 
     update: function () {
-      this.game.physics.arcade.collide(this.player, this.layer);
-      this.layer.position.y += 100;
+      var that = this;
+      this.game.physics.arcade.collide(this.player, this.layer, function(){
+        //TODO: buggy probably because of scale on player
+        //what about player body?
+        console.log('collision!!');
+
+        that.game.state.start('menu');
+      });
+
+      this.player.body.velocity.y = -250;
+
+      //this.game.camera.y -= 2;
+
+      //TODO: scale & velocity vs collistions....
+      ///this.player.y = this.game.camera.y + 50;
+
       //TODO: check time diff!!!!!
-      var speed = 0.01;
+      var speed = 0.06;
       if (this.game.input.mousePointer.isDown){
           this.player.scale.x *= 1 + speed;
           this.player.scale.y *= 1 + speed;
       }else{
           this.player.scale.x *= 1 - speed;
           this.player.scale.y *= 1 - speed;
+      }
+
+      if(this.player.scale.x < 0.1){
+        this.game.state.start('menu');
       }
     }
 
