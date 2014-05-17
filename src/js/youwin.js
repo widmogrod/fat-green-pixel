fcd1d487de2a7a49dabcef8659c958baaa72c9b4
widmogrod <widmogrod@gmail.com>
2014-05-17 21:08:42 +0200

    add YouWin state

---
+++ src/js/youwin.js
@@ -0,0 +1,46 @@
+(function() {
+  'use strict';
+
+  function YouWon() {
+    this.explanationTxt = null;
+    this.explanationTxt = null;
+    this.startTxt = null;
+  }
+
+  YouWon.prototype = {
+
+    create: function () {
+      var x = this.game.width / 2
+        , y = this.game.height / 2;
+
+      this.titleTxt = this.add.bitmapText(x, y, 'minecraftia', 'TRY AGAIN' );
+      this.titleTxt.align = 'center';
+      this.titleTxt.x = this.game.width / 2 - this.titleTxt.textWidth / 2;
+
+      var hintY = this.game.height / 4;
+      this.explanationTxt = this.add.bitmapText(x, hintY, 'minecraftia', 'TIP: Tap and hold to grow!' );
+      this.explanationTxt.align = 'center';
+      this.explanationTxt.x = this.game.width / 2 - this.explanationTxt.textWidth / 2;
+
+      y = y + this.explanationTxt.height + 5;
+      this.startTxt = this.add.bitmapText(x, y, 'minecraftia', 'You win');
+      this.startTxt.align = 'center';
+      this.startTxt.x = this.game.width / 2 - this.startTxt.textWidth / 2;
+
+      this.input.onDown.add(this.onDown, this);
+    },
+
+    update: function () {
+
+    },
+
+    onDown: function () {
+      this.game.state.start('game');
+    }
+  };
+
+  window['fat-green-pixel'] = window['fat-green-pixel'] || {};
+  window['fat-green-pixel'].YouWon = YouWon;
+
+}());
+
