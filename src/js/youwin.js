0827920579d2f4c4acb35d1d36b109627db26cf6
widmogrod <widmogrod@gmail.com>
2014-05-17 21:34:34 +0200

    best score

--- src/js/youwin.js
+++ src/js/youwin.js
@@ -6,19 +6,24 @@
     this.explanationTxt = null;
     this.startTxt = null;
     this.score = 0;
+    this.bestScore = 0;
+    this.beatScore = false;
   }
 
   YouWin.prototype = {
     init: function(score) {
-        if (score > this.score) {
-            this.score = score;
+        this.score = score;
+        this.beatScore = false;
+        if (score > this.bestScore) {
+            this.beatScore = true;
+            this.bestScore = score;
         }
     },
     create: function () {
       var x = this.game.width / 2
         , y = this.game.height / 2;
 
-      this.titleTxt = this.add.bitmapText(x, y, 'minecraftia', 'YOUR BEST SCORE: ' + this.score );
+      this.titleTxt = this.add.bitmapText(x, y, 'minecraftia', 'YOUR BEST SCORE: ' + this.bestScore);
       this.titleTxt.align = 'center';
       this.titleTxt.x = this.game.width / 2 - this.titleTxt.textWidth / 2;
 
@@ -26,6 +31,13 @@
       this.explanationTxt = this.add.bitmapText(x, hintY, 'minecraftia', 'Tap to beat you record!' );
       this.explanationTxt.align = 'center';
       this.explanationTxt.x = this.game.width / 2 - this.explanationTxt.textWidth / 2;
+
+      if (this.beatScore) {
+          this.explanationTxt.text = 'You beat your record!!'
+          this.explanationTxt.tint = 0xff33cc;
+      } else {
+          this.explanationTxt.text = 'You neet to try harder, colected: ' + this.score;
+      }
 
       y = y + this.explanationTxt.height + 5;
       this.startTxt = this.add.bitmapText(x, y, 'minecraftia', 'You win');
